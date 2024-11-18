57dce2a9ddce3f296c9c733d57253a96b0616df7
andreigerashchenko <67013056+andreigerashchenko@users.noreply.github.com>
2024-11-17 21:50:32 -0500

    Adjust temperature thresholds

--- src/cvc/misc.c
+++ src/cvc/misc.c
@@ -6,8 +6,8 @@
  */
 #include <cvc/data.h>
 #include <cvc/misc.h>
-#include <cvc/relay.h>
 #include <cvc/parse.h>
+#include <cvc/relay.h>
 #include <cvc/statemachine.h>
 #include <main.h>
 
@@ -16,15 +16,19 @@ void CVC_Cooling_Task() {
     CAN_Parse_EMUS_CellModuleTemperatureOverallParameters();
     CAN_Parse_Inverter_Temp1(0);
     CAN_Parse_Inverter_Temp1(1);
+    CAN_Parse_Inverter_Temp3TorqueShudder(0);
+    CAN_Parse_Inverter_Temp3TorqueShudder(1);
 
     int16_t max_cell_temp = CVC_data[BMS_MAX_CELL_TEMP] - 100;
-    int16_t max_module_temp = CVC_data[BMS_MAX_CELL_MODULE_TEMP] - 100;
+    int16_t max_bms_temp = CVC_data[BMS_MAX_CELL_MODULE_TEMP] - 100;
     float inverter1_temp_a = (float)((int16_t)CVC_data[INVERTER1_POWER_MODULE_A_TEMP]) / 10;
     float inverter1_temp_b = (float)((int16_t)CVC_data[INVERTER1_POWER_MODULE_B_TEMP]) / 10;
     float inverter1_temp_c = (float)((int16_t)CVC_data[INVERTER1_POWER_MODULE_C_TEMP]) / 10;
     float inverter2_temp_a = (float)((int16_t)CVC_data[INVERTER2_POWER_MODULE_A_TEMP]) / 10;
     float inverter2_temp_b = (float)((int16_t)CVC_data[INVERTER2_POWER_MODULE_B_TEMP]) / 10;
     float inverter2_temp_c = (float)((int16_t)CVC_data[INVERTER2_POWER_MODULE_C_TEMP]) / 10;
+    float motor1_temp = (float)((int16_t)CVC_data[INVERTER1_MOTOR_TEMP]) / 10;
+    float motor2_temp = (float)((int16_t)CVC_data[INVERTER2_MOTOR_TEMP]) / 10;
 
     float max_inverter1_temp = inverter1_temp_a;
     if (inverter1_temp_b > max_inverter1_temp) {
@@ -42,8 +46,6 @@ void CVC_Cooling_Task() {
         max_inverter2_temp = inverter2_temp_c;
     }
 
-    
-
     // Cooling pumps should be on any time inverters are on
     if (CVC_data[CVC_STATE] == READY_TO_DRIVE) {
         Relay_Set(Pumps, 1);
@@ -51,15 +53,15 @@ void CVC_Cooling_Task() {
         Relay_Set(Pumps, 0);
     }
     // Battery fans
-    if (max_cell_temp >= BATTERY_FAN_COOLING_TEMP || max_module_temp >= BATTERY_FAN_COOLING_TEMP) {
+    if (max_cell_temp >= CELL_TEMP_THRESHOLD || max_bms_temp >= BMS_TEMP_THRESHOLD) {
         Relay_Set(BatteryFans, 1);
-    } else if ((max_cell_temp < BATTERY_FAN_COOLING_TEMP - BATTERY_FAN_HYSTERESIS) && (max_module_temp < BATTERY_FAN_COOLING_TEMP - BATTERY_FAN_HYSTERESIS)) {
+    } else if ((max_cell_temp < CELL_TEMP_THRESHOLD - CELL_TEMP_HYSTERESIS) && (max_bms_temp < BMS_TEMP_THRESHOLD - BMS_TEMP_HYSTERESIS)) {
         Relay_Set(BatteryFans, 0);
     }
     // Radiator fans
-    if (max_inverter1_temp >= RADIATOR_FAN_COOLING_TEMP || max_inverter2_temp >= RADIATOR_FAN_COOLING_TEMP) {
+    if (max_inverter1_temp >= INVERTER_TEMP_THRESHOLD || max_inverter2_temp >= INVERTER_TEMP_THRESHOLD || motor1_temp >= MOTOR_TEMP_THRESHOLD || motor2_temp >= MOTOR_TEMP_THRESHOLD) {
         Relay_Set(Fans, 1);
-    } else if ((max_inverter1_temp < RADIATOR_FAN_COOLING_TEMP - RADIATOR_FAN_HYSTERESIS) && (max_inverter2_temp < RADIATOR_FAN_COOLING_TEMP - RADIATOR_FAN_HYSTERESIS)) {
+    } else if ((max_inverter1_temp < INVERTER_TEMP_THRESHOLD - INVERTER_TEMP_HYSTERESIS) && (max_inverter2_temp < INVERTER_TEMP_THRESHOLD - INVERTER_TEMP_HYSTERESIS) && (motor1_temp < MOTOR_TEMP_THRESHOLD - MOTOR_TEMP_HYSTERESIS) && (motor2_temp < MOTOR_TEMP_THRESHOLD - MOTOR_TEMP_HYSTERESIS)) {
         Relay_Set(Fans, 0);
     }
 
